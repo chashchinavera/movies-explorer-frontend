5b6bb6d455cf352df73ac88b932d2ed7e97d2750
Vera Chashchina <chaschinavera@gmail.com>
2023-08-08 12:34:02 +0300

    Update Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Main from '../Main/Main';
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
         <Routes>
             <Route
                 path='/'
+                element={<Main/>}
             />
             <Route
                 path='/movies'
