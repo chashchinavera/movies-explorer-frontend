e9104101fee9a026ca8545c5235825dd55b8d23c
Vera Chashchina <chaschinavera@gmail.com>
2023-08-09 13:08:02 +0300

    Update Promo

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,14 +1,12 @@
-import { Link } from 'react-router-dom';
-
 function Promo() {
     return (
         <section className='promo'>
             <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
-            <div className='promo__links'>
-            <Link to='#' className='promo__button'>О проекте</Link>
-            <Link to='#' className='promo__button'>Технологии</Link>
-            <Link to='#' className='promo__button'>Студент</Link>
-            </div>
+            <ul className='promo__links'>
+            <li><a href="#project" className='promo__button'>О проекте</a></li>
+            <li><a href="#" className='promo__button'>Технологии</a></li>
+            <li><a href="#" className='promo__button'>Студент</a></li>
+            </ul>
         </section>
     )
 }
