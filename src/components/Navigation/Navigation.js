5870091f04eb8a168dafa34e374f33dc5448578f
Vera Chashchina <chaschinavera@gmail.com>
2023-08-11 14:29:01 +0300

    Update Novigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
         <div className='navigation'>
             <div className='navigation__movies'>
                 <Link to='/movies' className='navigation__link'>Фильмы</Link>
-                <Link to='/movies' className='navigation__link navigation__movie'>Сохраненные фильмы</Link>
+                <Link to='/saved-movies' className='navigation__link navigation__movie'>Сохраненные фильмы</Link>
             </div>
             <Link to='/profile' className='navigation__link navigation__account'>Аккаунт</Link>
         </div>
