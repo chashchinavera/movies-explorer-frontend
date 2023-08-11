db44c3c9849736842eb28ab79e721ff33ca0eceb
Vera Chashchina <chaschinavera@gmail.com>
2023-08-11 23:17:12 +0300

    Update header__lago.svg

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo from '../../images/header__logo.png';
+import logo from '../../images/header__logo.svg';
 import Navigation from '../Navigation/Navigation';
 
 function Header({ loggedIn }) {
