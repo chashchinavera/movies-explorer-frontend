a9f604c97334d96f715184a487e3dece292b2ec3
Vera Chashchina <chaschinavera@gmail.com>
2023-08-13 10:17:05 +0300

    Add NotFound 1280px

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,37 +5,42 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
 
     return (
         <div className="page">
             <Routes>
-            <Route
-                path='/'
-                element={<Main/>}
-            />
-            <Route
-                path='/movies'
-                element={<Movies/>}
-            />
-            <Route
-                path='/saved-movies'
-                element={<SavedMovies />}
-            />
-            <Route
-                path='/profile'
-                element={<Profile />}
-            />
-            <Route
-                path='/signin'
-                element={<Login />}
-            />
-            <Route
-                path='/signup'
-                element={<Register />}
-            />
-        </Routes>
+                <Route
+                    path='/'
+                    element={<Main />}
+                />
+                <Route
+                    path='/movies'
+                    element={<Movies />}
+                />
+                <Route
+                    path='/saved-movies'
+                    element={<SavedMovies />}
+                />
+                <Route
+                    path='/profile'
+                    element={<Profile />}
+                />
+                <Route
+                    path='/signin'
+                    element={<Login />}
+                />
+                <Route
+                    path='/signup'
+                    element={<Register />}
+                />
+                <Route
+                    path='*'
+                    element={<NotFound />}
+                />
+            </Routes>
         </div>
     )
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+function NotFound () {
+    
+    return (
+        <section className='unfound'>
+<h2 className='unfound__error'>404</h2>
+<p className='unfound__text'>Страница не найдена</p>
+<a href='/' className='unfound__link'>Назад</a>
+        </section>
+    )
+}
+
+export default NotFound;
