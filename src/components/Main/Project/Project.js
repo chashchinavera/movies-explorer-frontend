88c81db0e28b727f843578fc682e2882a1ac75ed
Vera Chashchina <chaschinavera@gmail.com>
2023-08-13 11:52:35 +0300

    Update Project

--- src/components/Main/Project/Project.js
+++ src/components/Main/Project/Project.js
@@ -4,11 +4,11 @@ function Project() {
         <section className='project' id="project">
             <h2 className='project__title'>О проекте</h2>
             <div className='project__about'>
-                <div>
+                <div className='project__block'>
                     <h3 className='project__subtitle'>Дипломный проект включал 5 этапов</h3>
                     <p className='project__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
                 </div>
-                <div >
+                <div className='project__block'>
                     <h3 className='project__subtitle'>На выполнение диплома ушло 5 недель</h3>
                     <p className='project__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
                 </div>
